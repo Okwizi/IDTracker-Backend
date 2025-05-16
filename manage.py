eba180e633092cc03e93516c66f9171889a1b1f8
Okwizi <okwizifr@gmail.com>
2025-05-16 15:57:57 +0300

    chore: add test suite

--- manage.py
+++ manage.py
@@ -7,7 +7,14 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "id_tracker.config.settings")
+    if "test" in sys.argv:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "tests.config.settings"
+        )
+    else:
+        os.environ.setdefault(
+            "DJANGO_SETTINGS_MODULE", "id_tracker.config.settings"
+        )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
